f472b6e86f2583dea2dde2658ddd2ddc52aa780a
陈帅 <rockychen7@gmail.com>
2017-04-13 00:22:14 +0800

    add hide/publish button

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -45,10 +45,11 @@ class Admin::JobsController < ApplicationController
   end
 
   def require_is_admin
-    unless current_user.admin?
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
-    redirect_to root
+    redirect_to root_path
   end
+end
 
   private
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   def admin?
-  email == 'xiite@growth.school'
+  is_admin
   end
 end
