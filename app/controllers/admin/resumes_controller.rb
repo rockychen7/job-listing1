015c080478d32021d36cfa63beaafe8a081fda42
陈帅 <rockychen7@gmail.com>
2017-04-16 21:43:33 +0800

    all done

--- app/controllers/admin/resumes_controller.rb
+++ app/controllers/admin/resumes_controller.rb
@@ -5,7 +5,7 @@ class Admin::ResumesController < ApplicationController
   layout 'admin'
 
   def index
-    @job = Job.find(params[:job])
-    @resumes = @job.order('created_at DESC')
+    @job = Job.find(params[:job_id])
+    @resumes = @job.resumes.order('created_at DESC')
   end
 end
